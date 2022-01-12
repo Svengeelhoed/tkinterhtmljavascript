2bdf0738219d65e8b31bef6fa17becb24e1392eb
svengeelhoed <99069588@mydavinci.nl>
2022-01-12 12:05:57 +0100

    lichtschakelaar javascript af

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 var btn = document.createElement('button');
 btn.style.margin = '10px';
-btn.innerHTML = '...';
+btn.innerHTML = 'Licht uit';
 document.body.appendChild(btn);
 
 // schijf hier tussen je code
+HuidigeKleur = "yellow"
+document.body.style.background = "yellow";
+
+function Achtergrond() { 
+    btn.innerHTML = ""
+    if (HuidigeKleur == "black"){
+        btn.innerHTML = "Licht uit"
+        HuidigeKleur = "yellow" 
+         console.log("Licht aan")
+    }else{
+    HuidigeKleur = "black"
+     console.log("Licht uit")
+     btn.innerHTML = "Licht aan"
+    }document.body.style.background = HuidigeKleur;}
 
 
-
+btn.onclick = function(){ 
+    Achtergrond(HuidigeKleur)
+}
 // schijf hier tussen je code
